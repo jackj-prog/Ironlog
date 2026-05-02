dcc55e50dca1837cd364d68365f3c2e617f49430
jackj-prog <jackjcj78@gmail.com>
2026-05-02 11:27:16 +0100

    Delete src directory

--- src/components/SummaryCard.jsx
+++
@@ -1,8 +0,0 @@
-export function SummaryCard({ label, value, tone = 'default' }) {
-  return (
-    <article className={`summary-card ${tone}`}>
-      <span>{label}</span>
-      <strong>{value}</strong>
-    </article>
-  );
-}
